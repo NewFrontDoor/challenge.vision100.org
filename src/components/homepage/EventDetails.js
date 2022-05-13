2e876990a5946c29204f54e4afc552ced713915b
Bratalix <bratalix@gmail.com>
2022-05-13 12:35:49 +1000

    requested change

--- src/components/homepage/EventDetails.js
+++ src/components/homepage/EventDetails.js
@@ -1,32 +1,34 @@
 /* eslint-disable */
-import React, { Component } from 'react';
-import _ from 'lodash';
-
+import React, { Component } from "react";
+import _ from "lodash";
 
 class EventDetails extends Component {
-  render() {
-    return (
-      <section>
-        <div className="col-md-4 col-xs-12">  <div className="region region-content-2-3">
+	render() {
+		return (
+			<section>
+				<div className="col-md-4 col-xs-12">
+					{" "}
+					<div className="region region-content-2-3">
+						<div className="block block-block">
+							<h2 className="header">When</h2>
 
-          <div className="block block-block">
-
-            <h2 className="header">When</h2>
-
-            <div className="content">
-              <p><strong>Dates</strong></p>
-              <p>Friday 12th - Saturday 13th August 2022</p>
-              <p><strong>Times</strong></p>
-              <p>Friday 1:30pm - 8:30pm</p>
-              <p>Saturday 8:30am - 5:30pm</p>
-            </div>
-          </div>
-        </div>
-        </div >
-
-      </section >
-    );
-  }
+							<div className="content">
+								<p>
+									<strong>Dates</strong>
+								</p>
+								<p>Friday 12th - Saturday 13th August 2022</p>
+								<p>
+									<strong>Times</strong>
+								</p>
+								<p>Friday 1:30pm - 8:30pm</p>
+								<p>Saturday 9:30am - 5:30pm</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default EventDetails;
