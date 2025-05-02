1b3d8d6b1b98b3670e10f8be147521b51749089b
Will Ho <wkayi076@gmail.com>
2025-05-02 09:43:07 +0800

    Change description

--- src/components/homepage/WhatIsChallenge.jsx
+++ src/components/homepage/WhatIsChallenge.jsx
@@ -6,11 +6,13 @@ function WhatIsChallenge() {
 			<div>
 				<h2 className="gradient-text">What is Challenge?</h2>
 				<p className="gradient-text">
-					Challenge Conference provides the <em>challenge</em> to start the journey of exploring gospel ministry and
+					{/* Challenge Conference provides the <em>challenge</em> to start the journey of exploring gospel ministry and
 					help with the first steps toward raising up the next missionaries, pastors, church planters and women's
 					workers for Tasmania and beyond. Challenge is the place of <em>refreshment</em>, <em>encouragement</em> and{" "}
 					<em>inspiration</em> for all of us who are faithfully serving in our local churches or Christian organisations
-					to keep going and keep growing in our various ministries.{" "}
+					to keep going and keep growing in our various ministries.{" "} */}
+					Challenge is a place of refreshment, equipping and camaraderie for all who lead and service in our local churches and Christian organisations. 
+					Challenge provides the encouragement and invitation to start a journey of exploring gospel work and to take steps toward being the next formal ministry and mission workers for Tasmania and beyond.
 				</p>
 			</div>
 		</section>
